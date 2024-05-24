ce97405966ffeae1f14632c5746610acda4e9903
HiroMooshroom <124800222+HiroMooshroom@users.noreply.github.com>
2024-05-24 16:16:20 +0200

    Meer pogingen op character controller
    Gefaald ;-;

--- Assets/Scipt/Contoller.cs
+++ Assets/Scipt/Contoller.cs
@@ -6,16 +6,39 @@ using UnityEngine;
 
 public class Contoller : MonoBehaviour
 {
+    public float moveSpeed = 5f;
+    public float mouseSensitivity = 100f;
+    public CharacterController controller;
+    public Transform playerCamera;
 
-    // Start is called before the first frame update
+    private float xRotation = 0f;
+
     void Start()
     {
-
+        if (controller == null)
+        {
+            controller = GetComponent<CharacterController>();
+        }
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // Mouse look
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        playerCamera.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+
+        // Player movement
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * moveX + transform.forward * moveZ;
+        controller.Move(move * moveSpeed * Time.deltaTime);
     }
 }
